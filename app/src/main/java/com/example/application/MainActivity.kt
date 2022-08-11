1a7f80f108d3f0d4e14b2bc0f22010bb8ef92de0
chrilona <christabelalona@gmail>
2022-08-11 13:16:58 +0300

    bottom app menu

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.application
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnMountain:Button
+    lateinit var imgMountain:ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnMountain =findViewById(R.id.btnMountain)
         btnMountain.setOnClickListener {
             val intent=Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+        imgMountain=findViewById(R.id.imgMountain)
+        imgMountain.setOnClickListener {
+            val intent =Intent(this,HomeActivity::class.java)
             startActivity(intent)
         }
     }
